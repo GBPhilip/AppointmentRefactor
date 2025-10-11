957eaed7d64df3a969ec93463e1dff85e64a0a76
Philip Sutton <GBPhilipSutton@gmail.com>
2025-10-11 20:52:05 +0100

    MoveAfterExisting Metting into own file

--- RefactorTests/DiaryControllerTests.cs
+++ RefactorTests/DiaryControllerTests.cs
@@ -36,29 +36,6 @@ namespace Refactor.Tests
         }
 
         [Fact]
-        public async Task ReturnsAvailableSlot_AfterExistingMeeting()
-        {
-            var day = new DateOnly(2025, 10, 8);
-            var slots = new List<DiarySlot>
-            {
-                new DiarySlot
-                {
-                    StartTime = day.ToDateTime(new TimeOnly(9, 0, 0)),
-                    EndTime = day.ToDateTime(new TimeOnly(9, 30, 0))
-                }
-            };
-            var context = GetInMemoryContext(slots);
-            var controller = new DiaryController(context);
-
-            var result = await controller.GetAvailableSlot(30, day, new TimeOnly(9, 0, 0), new TimeOnly(10, 0, 0));
-
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            var slot = Assert.IsType<AvailableSlotDto>(okResult.Value);
-            Assert.Equal(day.ToDateTime(new TimeOnly(9, 30, 0)), slot.Start);
-            Assert.Equal(day.ToDateTime(new TimeOnly(10, 0, 0)), slot.End);
-        }
-
-        [Fact]
         public async Task ReturnsBadRequest_WhenMinutesOutOfRange()
         {
             var context = GetInMemoryContext(new List<DiarySlot>());
---
+++ RefactorTests/GetAvailableSlotTests/ReturnsAvailableSlot/AfterExistingMeeting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Refactor.Tests.GetAvailableSlotTests.ReturnsAvailableSlot
+{
+    public class AfterExistingMeeting
+    {
+        private GetAvailableSlotTestHelpers GetAvailableSlotTestHelpers;
+
+        public AfterExistingMeeting()
+        {
+            GetAvailableSlotTestHelpers = new GetAvailableSlotTestHelpers();
+        }
+
+        [Fact]
+        public async Task ReturnsAvailableSlot_AfterExistingMeeting()
+        {
+            var day = new DateOnly(2025, 10, 8);
+            var slots = new List<DiarySlot>
+            {
+                new DiarySlot
+                {
+                    StartTime = day.ToDateTime(new TimeOnly(9, 0, 0)),
+                    EndTime = day.ToDateTime(new TimeOnly(9, 30, 0))
+                }
+            };
+            var context = GetAvailableSlotTestHelpers.GetInMemoryContext(slots);
+            var controller = new DiaryController(context);
+
+            var result = await controller.GetAvailableSlot(30, day, new TimeOnly(9, 0, 0), new TimeOnly(10, 0, 0));
+            var slot = (result as OkObjectResult).Value as AvailableSlotDto;
+            Assert.Multiple(() =>
+                {
+                    Assert.Equal(day.ToDateTime(new TimeOnly(9, 30, 0)), slot.Start);
+                    Assert.Equal(day.ToDateTime(new TimeOnly(10, 0, 0)), slot.End);
+                }
+            );
+        }
+    }
+}
