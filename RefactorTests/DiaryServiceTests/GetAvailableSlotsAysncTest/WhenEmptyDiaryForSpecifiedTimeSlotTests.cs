5591b47542d44e1c63336368402548a0a62399d6
Philip Sutton <Philip.Sutton@ensek.co.uk>
2025-10-31 16:27:26 +0000

    Add unit test for GetAvailableSlotAsync with Moq
    Introduce WhenEmptyDiaryForSpecifiedTimeSlotTests class to test
    GetAvailableSlotAsync method in DiaryService using Moq for
    dependency mocking. Update Refactor.Tests.csproj to include
    Moq package for creating mock objects.

---
+++ RefactorTests/DiaryServiceTests/GetAvailableSlotsAysncTest/WhenEmptyDiaryForSpecifiedTimeSlotTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+
+namespace Refactor.Tests.DiaryServiceTests.GetAvailableSlotsAysncTest
+{
+    public class WhenEmptyDiaryForSpecifiedTimeSlotTests
+    {
+
+        [Fact]
+        public async Task ReturnsAvailableSlot()
+        {
+            DateOnly testDay = new(2025, 10, 8);
+            DiaryDay testDiaryDay = new()
+            {
+                Date = new DateOnly(2025, 10, 8),
+                StartTime = new TimeOnly(9, 0, 0),
+                EndTime = new TimeOnly(17, 0, 0)
+            };
+
+            var diaryDayRepository = new Mock<IDiaryDayRepository>();
+            var diarySlotRepository = new Mock<IDiarySlotRepository>();
+            var sut = new DiaryService(diaryDayRepository.Object, diarySlotRepository.Object); 
+            diarySlotRepository.Setup(x => x.GetSlotsForDayAsync(testDay))
+                .ReturnsAsync([]);
+            var result = await sut.GetAvailableSlotAsync(25, testDay, new TimeOnly(9, 0, 0), new TimeOnly(10, 0, 0), testDiaryDay);
+
+            Assert.Multiple(() =>
+                {
+                    Assert.Equal(testDay.ToDateTime(new TimeOnly(9, 0, 0)), result.Start);
+                    Assert.Equal(testDay.ToDateTime(new TimeOnly(9, 25, 0)), result.End);
+                }
+            );
+        }
+    }
+}
